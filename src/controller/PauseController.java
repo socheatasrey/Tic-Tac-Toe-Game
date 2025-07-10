453a887d79d5a712a4340c067bd116901b6f1d01
MONYCC <monycc2024@gmail.com>
2025-07-11 04:25:04 +0700

    add pause system

--- src/controller/PauseController.java
+++ src/controller/PauseController.java
@@ -1,8 +1,5 @@
 package controller;
 
-
-import javax.sound.sampled.Clip;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,33 +11,44 @@ import util.SoundUtil;
 
 public class PauseController {
 
-    @FXML
-    private Button button_continue;
+    @FXML private Button button_continue;
+    @FXML private Button button_home;
+    @FXML private Button button_playagain;
+    @FXML private ToggleButton togglebutton_music;
+    @FXML private ToggleButton togglebutton_sound;
+
+    private String previousSceneFxml;
+
+    public void setPreviousScene(String fxml) {
+        this.previousSceneFxml = fxml;
+    }
 
     @FXML
-    private Button button_home;
+    public void initialize() {
+        togglebutton_music.setSelected(SoundUtil.isMusicMuted());
+        togglebutton_sound.setSelected(SoundUtil.isSoundMuted());
 
-    @FXML
-    private Button button_playagain;
-
-    @FXML
-    private ToggleButton togglebutton_music;
-
-    @FXML
-    private ToggleButton togglebutton_sound;
+        togglebutton_music.setText(SoundUtil.isMusicMuted() ? "OFF" : "ON");
+        togglebutton_sound.setText(SoundUtil.isSoundMuted() ? "OFF" : "ON");
+    }
 
     @FXML
     void button_continue_action(ActionEvent event) {
         SoundUtil.clik();
-
+        try {
+            Stage stage = (Stage) button_continue.getScene().getWindow();
+            Scene scene = FXMLLoader.load(getClass().getResource(previousSceneFxml));
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     void button_home_action(ActionEvent event) {
         SoundUtil.clik();
-
         try {
-
             Stage stage = (Stage) button_home.getScene().getWindow();
             Scene scene = FXMLLoader.load(getClass().getResource("/fxml/Home.fxml"));
             stage.setScene(scene);
@@ -48,32 +56,34 @@ public class PauseController {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
-
-    private Clip backgroundClip;
-    //private boolean isMuted = false;
 
     @FXML
     void button_playagain_action(ActionEvent event) {
         SoundUtil.clik();
-
-    }
-
-    @FXML
-    void togglebutton_music_action(ActionEvent event) {
-        if (togglebutton_music.isSelected()) {
-            backgroundClip.start();
-            togglebutton_music.setText("OFF");
-        } else {
-            backgroundClip.stop();
-            togglebutton_music.setText("ON");
+        try {
+            Stage stage = (Stage) button_playagain.getScene().getWindow();
+            Scene scene = FXMLLoader.load(getClass().getResource(previousSceneFxml));
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
     @FXML
-    void togglebutton_sound_action(ActionEvent event) {
-
+    void togglebutton_music_action(ActionEvent event) {
+        boolean isMuted = togglebutton_music.isSelected();
+        SoundUtil.setMusicMuted(isMuted);
+        togglebutton_music.setText(isMuted ? "OFF" : "ON");
+        SoundUtil.clik();
     }
 
+    @FXML
+    void togglebutton_sound_action(ActionEvent event) {
+        boolean isMuted = togglebutton_sound.isSelected();
+        SoundUtil.setSoundMuted(isMuted);
+        togglebutton_sound.setText(isMuted ? "OFF" : "ON");
+        SoundUtil.clik();
+    }
 }
